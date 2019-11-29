a42ab8220869e894a76e0702116eb5959dbc1ba5
Darshana Sen <darshanasen@Darshanas-MacBook-Pro.local>
2019-11-29 12:28:39 -0500

    app.js update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My EVEN Sweeter App
         </a>
       </header>
     </div>
